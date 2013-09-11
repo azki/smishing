dc74c68a1aab32457d01b675e257cecb7e4c6540
azki <azki@daumcorp.com>
2013-09-11 22:27:58 +0900

    good job.

--- src/org/azki/smishing/MainActivity.java
+++ src/org/azki/smishing/MainActivity.java
@@ -1,22 +1,113 @@
 package org.azki.smishing;
 
-import android.os.Bundle;
+import java.util.List;
+import java.util.Vector;
+
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.analytics.tracking.android.EasyTracker;
+import com.google.gson.Gson;
 
 public class MainActivity extends Activity {
+
+	private LayoutInflater mInflater;
+	private SharedPreferences pref;
+	private CustomAdapter mAdaptor;
+	private ListView blockedListView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		pref = getSharedPreferences("pref", Context.MODE_MULTI_PROCESS);
+		mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		blockedListView = (ListView) findViewById(R.id.blockedListView);
+
+		Vector<RowData> dataList = new Vector<RowData>();
+		Gson gson = new Gson();
+		int blockedCount = pref.getInt("blockedCount", 0);
+		for (int i = 0; i < blockedCount; i++) {
+			String rawJson = pref.getString("blocked" + i, null);
+			if (rawJson != null) {
+				dataList.add(gson.fromJson(rawJson, RowData.class));
+			}
+		}
+		mAdaptor = new CustomAdapter(this, android.R.layout.simple_list_item_1, dataList);
+		blockedListView.setAdapter(mAdaptor);
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		EasyTracker.getInstance().activityStart(this);
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		EasyTracker.getInstance().activityStop(this);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_list_clear:
+			clearBlockedList();
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	private void clearBlockedList() {
+		int blockedCount = pref.getInt("blockedCount", 0);
+		Editor editor = pref.edit();
+		for (int i = 0; i < blockedCount; i++) {
+			editor.remove("blocked" + i);
+		}
+		editor.remove("blockedCount");
+		editor.commit();
+		mAdaptor.clear();
+		mAdaptor.notifyDataSetChanged();
+	}
+
+	private class CustomAdapter extends ArrayAdapter<RowData> {
+		public CustomAdapter(Context context, int textViewResourceId, List<RowData> objects) {
+			super(context, textViewResourceId, objects);
+		}
+
+		@Override
+		public View getView(int position, View convertView, final ViewGroup parent) {
+			final RowData rowData = getItem(position);
+			if (convertView == null) {
+				convertView = mInflater.inflate(R.layout.list_item, null);
+			}
+			TextView dateTextView = (TextView) convertView.findViewById(R.id.dateTextView);
+			TextView bodyTextView = (TextView) convertView.findViewById(R.id.bodyTextView);
+			TextView senderTextView = (TextView) convertView.findViewById(R.id.senderTextView);
+
+			dateTextView.setText(rowData.when);
+			bodyTextView.setText(rowData.body);
+			senderTextView.setText(rowData.sender);
+			return convertView;
+		}
+	}
 }
--- src/org/azki/smishing/SmsReceiver.java
+++ src/org/azki/smishing/SmsReceiver.java
@@ -3,24 +3,34 @@ package org.azki.smishing;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.Random;
 import java.util.regex.Matcher;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.support.v4.app.NotificationCompat;
 import android.telephony.SmsMessage;
 import android.util.Log;
 import android.widget.Toast;
+
+import com.google.analytics.tracking.android.EasyTracker;
+import com.google.gson.Gson;
 
 public class SmsReceiver extends BroadcastReceiver {
 	public static final String ACTION = "android.provider.Telephony.SMS_RECEIVED";
 
 	public void onReceive(Context context, Intent intent) {
-		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-		StrictMode.setThreadPolicy(policy);
-
 		if (intent.getAction().equals(ACTION)) {
 			Bundle bundle = intent.getExtras();
 			if (bundle != null) {
@@ -33,11 +43,24 @@ public class SmsReceiver extends BroadcastReceiver {
 						String msgBody = message.getMessageBody();
 
 						Matcher m = android.util.Patterns.WEB_URL.matcher(msgBody);
+						boolean canNetworkInMainThread = false;
 						while (m.find()) {
 							String urlStr = m.group();
 							if (urlStr.contains(".apk")) {
-								blockMsg(context, msgOriginating, msgBody);
+								blockMsg(context, msgBody, msgOriginating);
+								try {
+									EasyTracker.getInstance().setContext(context);
+									EasyTracker.getTracker().sendView("block by url");
+								} catch (Exception ex) {
+									Log.e("tag", "ga error", ex);
+								}
 							} else {
+								if (canNetworkInMainThread == false) {
+									StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll()
+											.build();
+									StrictMode.setThreadPolicy(policy);
+									canNetworkInMainThread = true;
+								}
 								try {
 									URL url;
 									try {
@@ -50,12 +73,23 @@ public class SmsReceiver extends BroadcastReceiver {
 									String contentType = connection.getContentType();
 									String contentDisposition = connection.getHeaderField("Content-Disposition");
 									connection.disconnect();
-									
+
 									if (contentType != null && contentType.contains("vnd.android.package-archive")) {
-										blockMsg(context, msgOriginating, msgBody);
-									}
-									if (contentDisposition != null && contentDisposition.contains(".apk")) {
-										blockMsg(context, msgOriginating, msgBody);
+										blockMsg(context, msgBody, msgOriginating);
+										try {
+											EasyTracker.getInstance().setContext(context);
+											EasyTracker.getTracker().sendView("block by contentType");
+										} catch (Exception ex) {
+											Log.e("tag", "ga error", ex);
+										}
+									} else if (contentDisposition != null && contentDisposition.contains(".apk")) {
+										blockMsg(context, msgBody, msgOriginating);
+										try {
+											EasyTracker.getInstance().setContext(context);
+											EasyTracker.getTracker().sendView("block by contentDisposition");
+										} catch (Exception ex) {
+											Log.e("tag", "ga error", ex);
+										}
 									}
 								} catch (Exception ex) {
 									Toast.makeText(context, ex.getMessage(), Toast.LENGTH_LONG).show();
@@ -69,9 +103,38 @@ public class SmsReceiver extends BroadcastReceiver {
 		}
 	}
 
-	private void blockMsg(Context context, String msgOriginating, String msgBody) {
+	private void blockMsg(Context context, String msgBody, String msgOriginating) {
 		abortBroadcast();
 
-		Toast.makeText(context, "차단된 메시지:\n" + msgBody + "\n" + msgOriginating, Toast.LENGTH_LONG).show();
+		RowData rowData = new RowData();
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd E HH:mm:ss", Locale.getDefault());
+		rowData.when = sdf.format(cal.getTime());
+		rowData.body = msgBody;
+		rowData.sender = msgOriginating;
+		Gson gson = new Gson();
+		String rawJson = gson.toJson(rowData);
+
+		SharedPreferences pref = context.getSharedPreferences("pref", Context.MODE_MULTI_PROCESS);
+		int blockedCount = pref.getInt("blockedCount", 0);
+		Editor editor = pref.edit();
+		editor.putString("blocked" + blockedCount, rawJson);
+		editor.putInt("blockedCount", blockedCount + 1);
+		editor.commit();
+
+		String title = context.getResources().getString(R.string.blocked_msg);
+		String text = context.getResources().getString(R.string.blocked_msg_detail, msgOriginating);
+
+		Intent intent = new Intent(context, MainActivity.class);
+		PendingIntent resultPendingIntent = PendingIntent.getActivity(context, new Random().nextInt(), intent,
+				PendingIntent.FLAG_UPDATE_CURRENT);
+		NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+				.setSmallIcon(R.drawable.ic_launcher).setTicker(title).setContentTitle(title).setContentText(text)
+				.setAutoCancel(true).setContentIntent(resultPendingIntent);
+		Notification notification = mBuilder.build();
+		notification.defaults = Notification.DEFAULT_ALL;
+		notification.flags = Notification.FLAG_SHOW_LIGHTS;
+		nm.notify(new Random().nextInt(), notification);
 	}
 }
