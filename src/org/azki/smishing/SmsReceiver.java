4e627d193e9be7f344385af117fe54bdbc524671
azki <azki@daumcorp.com>
2013-09-23 10:17:34 +0900

    Organize Imports

--- src/org/azki/smishing/SmsReceiver.java
+++ src/org/azki/smishing/SmsReceiver.java
@@ -3,7 +3,6 @@ package org.azki.smishing;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
